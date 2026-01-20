0b026632a7197693718ad2222c8b594ff4a8b8f4
“ThanhTo04” <thanhtiti04@gmail.com>
2026-01-20 17:42:17 +0700

    demo controller

---
+++ MvcMovie/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace MvcMovie.Controllers
+{
+    public class DemoController : Controller
+    { 
+       public IActionResult Index()
+       {
+        return View();
+       }
+    }
+}
